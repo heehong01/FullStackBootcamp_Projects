f5e729c3ef053f274296b49595acc8e49473f8b2
heehong01 <heejin.joanna.hong@gmail.com>
2022-03-15 16:23:23 -0500

    ABSTRACT DAO

---
+++ AbstractDAOActivity/src/main/java/com/AbstractDAOActivity/DAOMain.java
@@ -0,0 +1,123 @@
+package com.heejinhong.AbstractDAOActivity;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.AbstractDAOActivity.Customer.*;
+import com.AbstractDAOActivity.Item.*;
+import com.AbstractDAOActivity.mydb.*;
+
+public class DAOMain {
+    public static void main(String[] args) {
+
+        //Initialization of Customers and Item table
+        initializationOfTablesCustomersAndItem();
+
+        insertSomeDataIntoTablesCustomerAndItem();
+
+        CustomerDAOI customerDAOI = new CustomerDAOImpl();
+        ItemDAOI itemDAOI = new ItemDAOImpl();
+
+
+        itemInsertDeleteAndGetAllItemFunctions(itemDAOI);
+
+        customerGetAddAndRemoveCustomer(customerDAOI);
+
+        removingTables();
+    }
+
+    private static void customerGetAddAndRemoveCustomer(CustomerDAOI customerDAOI) {
+        System.out.println("Getting Customer by ID ");
+
+        Customer customer = customerDAOI.getCustomerById(5);
+        printCustomer(customer);
+
+        System.out.println();
+        System.out.println("Adding New Customer");
+
+        Customer addCustomer = new Customer(6, "heehong01@gmail.com", "Heejin", "Hong");
+        customerDAOI.addCustomer(addCustomer);
+
+        System.out.println();
+        System.out.println("Removing Customer by ID");
+
+        customerDAOI.removeCustomerById(1);
+
+        System.out.println("Getting customer at ID that is removed and seeing if there is any value");
+        Customer removedcustomer = customerDAOI.getCustomerById(1);
+        printCustomer(removedcustomer);
+
+        System.out.println();
+    }
+
+
+
+    private static void itemInsertDeleteAndGetAllItemFunctions(ItemDAOI itemDAOI) {
+        System.out.println("Getting all items");
+        List<Item> getAllItem = itemDAOI.getAllItems();
+        printItem(getAllItem);
+
+        System.out.println();
+        System.out.println("Inserting new Item");
+
+        Item insert = new Item(5, "FIVE WATT", 16.99);
+        itemDAOI.addItem(insert);
+        List<Item> allItems = itemDAOI.getAllItems();
+        printItem(allItems);
+
+        System.out.println();
+        System.out.println("Removing an Item by ID");
+
+        itemDAOI.removeItemById(4);
+        List<Item> allItemsUpdated = itemDAOI.getAllItems();
+        printItem(allItemsUpdated);
+
+        System.out.println();
+    }
+
+    private static void printItem(List<Item> allItems) {
+        allItems.forEach(item
+                -> System.out.println("Item info: "
+                + item.getId() + " "
+                + item.getName() + " "
+                + item.getPrice()));
+        System.out.println();
+    }
+    private static void printCustomer(Customer customer) {
+        System.out.println("Customer at ID: " + customer.getId()
+                + " | " + customer.getEmail()
+                + " | " + customer.getFName()
+                + " | " + customer.getLName());
+        System.out.println();
+    }
+
+
+    private static void initializationOfTablesCustomersAndItem() {
+        CreateCustomersTable.createCustomersTable();
+        CreateItemsTable.createItemsTable();
+    }
+
+    private static void insertSomeDataIntoTablesCustomerAndItem() {
+        InsertCustomersTable.insertCustomersTable();
+        InsertItemsTable.insertItemsTable();
+    }
+
+    private static void removingTables() {
+        DropCustomerTable.dropCustomerTable();
+        DropItemTable.dropItemTable();
+    }
+}
+/*Database Tables:
+Customer - id(int), email(varchar2), fname(varchar2), lname(varchar2).
+Item - id(int), name(varchar2), price(Number(4,2)).
+Create models for each table.
+Create DAOs with the following methods:
+CustomerDAO
+getCustomerById(int id) - Returns the customer object for the given id.
+addCustomer(Customer c) - Adds a customer with given information.
+removeCustomerById(int id) - Removes a customer with the given id.
+ItemDAO
+getAllItems() - Returns a list of all item objects in the database.
+addItem(Item i) - Adds an item with given information.
+removeItemById(int id) - Removes an item with the given id.
+*/
---
+++ AbstractDAOActivity/src/main/java/com/AbstractDAOActivity/Item/ItemDAOI.java
@@ -0,0 +1,25 @@
+package com.heejinhong.AbstractDAOActivity.Item;
+
+import java.sql.SQLTransientConnectionException;
+import com.heejinhong.AbstractDAOActivity.Item.Item;
+
+import java.util.List;
+
+public interface ItemDAOI {
+    enum SQL{
+        GET_ALL_ITEM("SELECT * FROM item"),
+        ADD_ITEM("INSERT INTO item(id, name, price) VALUES(?, ?, ?)"),
+        REMOVE_ITEM_BY_ID("DELETE FROM item where id = ?");
+        private final String query;
+
+        private SQL(String s) {
+            this.query = s;
+        }
+        public String getQuery() {
+            return query;
+        }
+    }
+    List<Item> getAllItems();
+    void addItem(Item i);
+    void removeItemById(int id);
+}
