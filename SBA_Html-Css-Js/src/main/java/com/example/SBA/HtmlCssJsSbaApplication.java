fbca2026fc9d0e6e60f9c28fbd6c03b2b959c1f3
heehong01 <heejin.joanna.hong@gmail.com>
2022-03-28 16:38:30 -0500

    SBA on HTML CSS AND JS

---
+++ SBA_Html-Css-Js/src/main/java/com/example/SBA/HtmlCssJsSbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlCssJsSbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlCssJsSbaApplication.class, args);
+	}
+
+}
