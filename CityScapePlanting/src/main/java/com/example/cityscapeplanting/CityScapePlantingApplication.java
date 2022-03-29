77bfe32f91cb7c95d54aa0328a350aebd255eb46
heehong01 <heejin.joanna.hong@gmail.com>
2022-03-29 16:07:22 -0500

    capstone addition

---
+++ CityScapePlanting/src/main/java/com/example/cityscapeplanting/CityScapePlantingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityscapeplanting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityScapePlantingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityScapePlantingApplication.class, args);
+    }
+
+}
